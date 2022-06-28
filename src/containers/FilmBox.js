a9777c6d386685817fda8d93009aac98b9137906
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-28 16:31:54 +0100

    mock data added to filmbox container

--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 const FilmBox = () => {
-
+    const [films, setFilms] = useState(
+        [
+            {
+                id: 1,
+                name: "Spider-Man: Into the Spider-Verse",
+                url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+            },
+            {
+                id: 2,
+                name: "Life Itself",
+                url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+            },
+            {
+                id: 3,
+                name: "Mary Queen of Scots",
+                url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+            },
+            {
+                id: 4,
+                name: "The Lego Movie 2: The Second Part", 
+                url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+            },
+            {
+                id: 5,
+                name: "Captain Marvel",
+                url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+            }
+        ]
+    )
 }
 
 export default FilmBox;
